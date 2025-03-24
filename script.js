5e6e7dd5822d253d56fcad37f1e233ff3ff69671
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-24 17:24:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let arr = [1, 2, 3, 4]
+let output = document.querySelector("#output");
+
+let myPromise = new Promise((res, rej) => {
+	setTimeout(()=>{
+		res(arr) 
+	}, 3000)
+})
+
+myPromise.then((arr) =>{
+	let evenArr = arr.filter(elm => elm%2==0)
+	return new Promise((res) => {
+		setTimeout(()=>{
+			output.innerText = evenArr;
+			res(evenArr)
+		}, 1000)
+	})
+	
+}).then((arr)=>{
+	let mulArr = arr.map(elm => elm*2)
+	setTimeout(()=>{
+		output.innerText = mulArr;
+	}, 2000)
+})
+
+
+
